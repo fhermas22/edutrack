c511b3204f9e273d677d3a110513daf189abcbce
Hermas Francisco <franciscohermas@gmail.com>
2025-12-22 08:57:53 +0100

    Add global app routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'welcome';
+    return view('layouts.app', [
+        'views' => [
+            'dashboard' => view('pages.dashboard')->render(),
+            'learners' => view('pages.learners')->render(),
+            'courses' => view('pages.courses')->render(),
+            'progress' => view('pages.progress')->render(),
+            'reports' => view('pages.reports')->render(),
+        ]
+    ]);
 });
